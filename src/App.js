d6e192fa51e606a80339e6af14e1e1a17dbaad50
Artem Murzo <murzotom@rocketmail.com>
2020-06-04 15:55:26 -0400

    fixed https for images

--- src/App.js
+++ src/App.js
@@ -177,7 +177,7 @@ function App() {
                 }}
               >
                 <img
-                  src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
                   alt={weather.weather[0].description}
                   style={{ width: 100, height: 100, margin: "auto" }}
                 />
